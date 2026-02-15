0cc6937b1df3e2910d20d05d639e21f08f946fd1
khodakivskiy <andriyhodak2007@gmail.com>
2026-02-15 14:50:17 +0200

    chore: refactor controller for using services instead of repos

--- backend/To Do List_ Project/Controllers/HomeController.cs
+++ backend/To Do List_ Project/Controllers/HomeController.cs
@@ -8,46 +8,55 @@ namespace todo.Controllers;
 public class HomeController : Controller
 {
     private readonly ITaskService _taskService;
+    private readonly ICategoryService _categoryService;
 
-    private readonly Repositories.SQLRepositories.SqlCategoryRepository _sqlCategoryRepository;
-
-    private readonly Repositories.XMLRepositories.XmlTaskRepository _xmlTaskRepository;
-    private readonly Repositories.XMLRepositories.XmlCategoryRepository _xmlCategoryRepository;
-
-    public HomeController(ITaskService taskService)
+    public HomeController(ITaskService taskService, ICategoryService categoryService)
     {
         _taskService = taskService;
+        _categoryService = categoryService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        var storage = GetCurrentStorage();
-        ViewBag.SelectedStorage = storage;
-
-        if(storage == "SQL")
-        {
-            var categories = _sqlCategoryRepository.GetCategories();
-            ViewBag.Categories = categories;
-
-            var activeTasks = _sqlTaskRepository.GetActiveTasks();
-            var completedTasks = _sqlTaskRepository.GetCompletedTasks();
-
-            ViewBag.activeTasks = activeTasks;
-            ViewBag.completedTasks = completedTasks;
-        }
-        else
-        {
-            var categories = _xmlCategoryRepository.GetCategories();
-            ViewBag.Categories = categories;
-
-            var activeTasks = _xmlTaskRepository.GetActiveTasks();
-            var completedTasks = _xmlTaskRepository.GetCompletedTasks();
-
-            ViewBag.activeTasks = activeTasks;
-            ViewBag.completedTasks = completedTasks;
-        }   
+        var storageType = GetCurrentStorage();
+        await InitPage(storageType);
 
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Task_Add(TaskModel task)
+    {
+        var storageType = GetCurrentStorage();
+        await _taskService.AddTaskAsync(task, storageType);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Is_Completed(int id)
+    {
+        var storageType = GetCurrentStorage();
+        await _taskService.MarkTaskAsCompleteAsync(id, storageType);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ClearTasks()
+    {
+        var storageType = GetCurrentStorage();
+        await _taskService.ClearTasksAsync(storageType);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Storage_Choose(string storage)
+    {
+        HttpContext.Session.SetString("StorageType", storage);
+
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -56,91 +65,17 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
-    [HttpPost]
-    public IActionResult Task_Add(TaskModel task)
-    {
-        if (ModelState.IsValid)
-        {
-            var storage = GetCurrentStorage();
 
-            if (storage == "SQL")
-                _sqlTaskRepository.AddTask(task);
-
-            else
-                _xmlTaskRepository.AddTask(task);
-
-            return RedirectToAction("Index");
-        }
-
-        return View();
-    }
-    [HttpPost]
-    public IActionResult Is_Completed(int taskId)
-    {
-        try
-        {
-            var storage = GetCurrentStorage();
-
-            TaskModel? task = storage == "SQL"
-        ? _sqlTaskRepository.GetTaskById(taskId)
-        : _xmlTaskRepository.GetTaskById(taskId);
-
-            if (task == null)
-                return NotFound();
-
-            task.Is_Completed = !task.Is_Completed;
-            task.Completed_At = DateTime.Now;
-
-            if (storage == "SQL")
-                _sqlTaskRepository.UpdateTask(task);
-            else
-                _xmlTaskRepository.UpdateTask(task);
-
-            return RedirectToAction("Index");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-            return View("Error");
-        }
-    }
-
-    [HttpPost]
-    public IActionResult CleanTasks()
-    {
-        try
-        {
-            var storage = GetCurrentStorage();
-
-            if (storage == "SQL")
-            {
-                _sqlTaskRepository.ClearTasks();
-            }
-            else
-            {
-                _xmlTaskRepository.ClearTasks();
-            }
-
-            return RedirectToAction("Index");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-            return View("Error");
-        }
-    }
-    [HttpPost]
-    public IActionResult Storage_Choose(string storage)
-    {
-        Console.WriteLine($"[Storage_Choose] Selected: {storage}");
-
-        if (storage=="SQL" || storage=="XML")
-            HttpContext.Session.SetString("StorageType", storage);
-
-        return RedirectToAction("Index");
-    }
     private string GetCurrentStorage()
     {
-        return HttpContext.Session.GetString("StorageType") ?? "SQL";
+        return HttpContext.Session.GetString("StorageType") ?? "sql";
+    }
+
+    private async Task InitPage(string storageType)
+    {
+        ViewBag.SelectedStorage = storageType;
+        ViewBag.Categories = await _categoryService.GetCategoriesAsync(storageType);
+        ViewBag.ActiveTasks = await _taskService.GetTasksByCompletionStatusAsync(false, storageType);
+        ViewBag.CompletedTasks = await _taskService.GetTasksByCompletionStatusAsync(true, storageType);
     }
 }
